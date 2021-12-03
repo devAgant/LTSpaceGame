e118fc2fb0f0d883ac3ac182e5a4b3d4fe644e8f
devAgant <68075877+devAgant@users.noreply.github.com>
2021-12-02 19:03:40 -0600

    Made Merchant Menu Working UI, Buy function not completed

---
+++ Assets/Scripts/NPC/Merchant.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Merchant : MonoBehaviour, IDragHandler, IPointerDownHandler
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject merchantMenu;
+    public GameObject merchant;
+    public KeyCode keyCode;
+    private bool isTriggered;
+    [SerializeField] private RectTransform dragRectTransform;
+    [SerializeField] private Canvas canvas;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        dragRectTransform.anchoredPosition += (eventData.delta / canvas.scaleFactor);
+    }
+
+
+    void Start()
+    {
+        isTriggered = merchant.gameObject.GetComponent<ProximDetect>().isTriggered;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isTriggered = merchant.gameObject.GetComponent<ProximDetect>().isTriggered;
+        if (Input.GetKeyDown(keyCode)) {
+            if (isTriggered == true) {
+                merchantMenu.gameObject.SetActive(!merchantMenu.gameObject.activeSelf);
+            }            
+        }
+        if (isTriggered == false) {
+            merchantMenu.gameObject.SetActive(false);
+        }
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+            dragRectTransform.SetAsLastSibling();
+    }
+}
