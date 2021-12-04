9680001cf8016640ebaae6f816d2ffcdd525afe2
Stepan Antonyuk <stepan.antonyuk@gmail.com>
2021-12-04 16:54:41 -0600

    Created CoinSpawner, can be attached to anything

---
+++ Assets/Scripts/Coin/CoinSpawnerV2.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawnerV2 : MonoBehaviour
+{
+
+    public Transform spawnPos;
+    public GameObject ItemToSpawn;
+    public int amount = 1;
+    public float HorDis = 2f;
+    public float VerDis = 2f;
+    //public GameObject Host;
+
+    /*    
+    private int Hp = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isDead();
+    }
+    
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    private void isDead()
+    {
+        if(Host == null || Hp == 0)
+        {
+            for(int i = 0; i < amount; i++)
+            {
+                Instantiate(ItemToSpawn, spawnPos.position, spawnPos.rotation);
+            }
+            Destroy(gameObject);
+        }
+    }
+    */
+    
+    private void OnDestroy()
+    {
+        for(int i = 0; i < amount; i++)
+        {
+            Instantiate(ItemToSpawn, spawnPos.position + new Vector3(Random.Range(-HorDis, HorDis), Random.Range(-VerDis, VerDis)), spawnPos.rotation);
+        }
+    }
+}
