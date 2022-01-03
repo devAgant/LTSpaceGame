c19faca189578fadb06fa839ff5c448d39d75720
Stepan Antonyuk <stepan.antonyuk@gmail.com>
2022-01-02 20:22:13 -0600

    Implemented Save and Load of player data

---
+++ Assets/Scripts/SaveLoad/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour // this is mor elike user, we save here stats of his character, level that he is playing, and items in his invetory, and so on...
+{
+    public int level = 0; // This is for testing
+    public int dexterity;
+    public int strength;
+    public int intelligence;
+    
+    public void SavePlayer ()
+    {
+        SaveSystem.SavePlayer(this);
+    }
+    
+    public void LoadPlayer ()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+        
+        level = data.level;
+	    dexterity = data.dex;
+	    strength = data.str;
+	    intelligence = data.inl;
+
+	    Vector3 position;
+	    position.x = data.position[0];
+	    position.y = data.position[1];
+	    position.z = data.position[2];
+	    transform.position = position;
+    }
+    
+}
